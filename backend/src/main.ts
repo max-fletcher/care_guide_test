61070292f90dd0490b4290dbb4c308d5ecd42e24
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2026-08-20 22:30:52 +0600

    Added port for render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,6 +21,8 @@ async function bootstrap() {
     type: VersioningType.URI
   })
 
-  await app.listen(process.env.PORT ?? 3000)
+  const port = process.env.PORT || 3000
+  await app.listen(port, '0.0.0.0')
+  console.log(`Backend running on http://localhost:${port}/api`)
 }
 bootstrap()
